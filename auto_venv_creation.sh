18c26454db5d0529e6f9d942f7777697103b146b
greg-buka <bukacieszuka1@gmail.com>
2023-04-17 20:00:39 +0200

    Add README, venv creation script

---
+++ auto_venv_creation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo '
+#####################################################
+	    	     Welcome!
+#####################################################
+I see you want to start a new Robot Framework project.
+   I will help you with create virtual environment.
+                     So lets begin!'
+
+echo 'Enter name to create project directory: '
+read PROJECT_DIR
+
+# Verify that directory with the user-specified name does not already exist in this path.
+
+[[ -d $PROJECT_DIR ]] && echo $PROJECT_DIR already exist, aborting && exit 1
+mkdir $PROJECT_DIR
+
+# Create python3 virtual environment
+
+virtualenv $PROJECT_DIR -p python3
+
+cd $PROJECT_DIR
+
+# Activate the virtual environment that was created
+
+source ./bin/activate
+
+
+
