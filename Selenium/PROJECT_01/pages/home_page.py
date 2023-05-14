83ac69725e308b4e2b7f534b1f0b44ce06b34039
greg-buka <bukacieszuka1@gmail.com>
2023-05-14 19:08:58 +0200

    Update pages

---
+++ Selenium/PROJECT_01/pages/home_page.py
@@ -0,0 +1,41 @@
+#import selenium
+from pages.base_page import BasePage
+from pages.sign_in_page import SignInPage
+from pages.create_account_page import CreateAccountPage
+
+from selenium.webdriver.common.by import By
+
+class Locators:
+    """Locators on home page"""
+    SIGN_IN_LINK = (By.LINK_TEXT, "Sign in")
+    CREATE_ACCOUNT_LINK = (By.LINK_TEXT, "Create an Account")
+    WHATS_NEW_PAGE = (By.ID, "ui-id-3")
+    WOMEN_PAGE = (By.ID, "ui-id-4")
+    MAN_PAGE = (By.ID, "ui-id-5")
+    GEAR_PAGE = (By.ID, "ui-id-6")
+    TRAINING_PAGE = (By.ID, "ui-id-7")
+    SALE_PAGE = (By.ID, "ui-id-8")
+
+class HomePage(BasePage):
+    """Main Page"""
+    def _verify_page(self):
+        pass
+    
+    def click_sign_in(self):
+        #1. Find element - (sign in)
+        el_sign_in = self.driver.find_element(Locators.SIGN_IN_LINK)
+        #2. Click sing in button
+        el_sign_in.click()
+        #3. Return log in page
+        return SignInPage(self.driver)
+    
+    def click_create_account(self):
+        #1. Find element - (create account)
+        el_crea_acc = self.driver.find_element(Locators.CREATE_ACCOUNT_LINK)
+        #2. Click 
+        el_crea_acc.click()
+        #3. Return log in page
+        return CreateAccountPage(self.driver)
+    
+
+    
