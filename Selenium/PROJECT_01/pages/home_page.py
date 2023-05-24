8a222935b2b81040c27cd55d8c69356cdc8b460a
greg-buka <bukacieszuka1@gmail.com>
2023-05-24 19:23:57 +0200

    Updated, fixed problem with import of modules, locators

--- Selenium/PROJECT_01/pages/home_page.py
+++ Selenium/PROJECT_01/pages/home_page.py
@@ -1,13 +1,13 @@
-#import selenium
-from base_page import BasePage
-from sign_in_page import SignInPage
-from create_account_page import CreateAccountPage
+from selenium import webdriver
+from pages.base_page import BasePage
+from pages.sign_in_page import SignInPage
+from pages.create_account_page import CreateAccountPage
 from selenium.webdriver.common.by import By
 
 class Locators:
     """Locators on Home Page"""
     SIGN_IN_LINK = (By.LINK_TEXT, "Sign in")
-    CREATE_ACCOUNT_LINK = (By.LINK_TEXT, "Create an Account")
+    CREATE_ACCOUNT_LINK = (By.XPATH, '//a[@href="https://magento.softwaretestingboard.com/customer/account/create/"]')
     WHATS_NEW_PAGE = (By.ID, "ui-id-3")
     WOMEN_PAGE = (By.ID, "ui-id-4")
     MAN_PAGE = (By.ID, "ui-id-5")
@@ -17,13 +17,11 @@ class Locators:
 
 class HomePage(BasePage):
     """Main Page"""
-    def _verify_page(self):
-        pass
     
     def click_sign_in(self):
         """Click link to sing in page"""
         #1. Find element - (sign in)
-        el_sign_in = self.driver.find_element(Locators.SIGN_IN_LINK)
+        el_sign_in = self.driver.find_element(*Locators.SIGN_IN_LINK)
         #2. Click sing in button
         el_sign_in.click()
         #3. Return log in page
@@ -32,7 +30,7 @@ class HomePage(BasePage):
     def click_create_account(self):
         """Click link to create account page"""
         #1. Find element - (create account)
-        el_crea_acc = self.driver.find_element(Locators.CREATE_ACCOUNT_LINK)
+        el_crea_acc = self.driver.find_element(*Locators.CREATE_ACCOUNT_LINK)
         #2. Click 
         el_crea_acc.click()
         #3. Return log in page
