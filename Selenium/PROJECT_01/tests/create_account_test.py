22b4d8c5719a4389f725a2fce7801c7a8b4ba8f1
greg-buka <bukacieszuka1@gmail.com>
2023-05-25 19:39:45 +0200

    Update create account tests

--- Selenium/PROJECT_01/tests/create_account_test.py
+++ Selenium/PROJECT_01/tests/create_account_test.py
@@ -14,22 +14,17 @@ class CreateAccountTest(BaseTest):
     def setUp(self):
         super().setUp()
         self.create_account_page = self.home_page.click_create_account()
-        sleep(5)
 
-#    def requiredFieldError(self, field_error_locator):
-#        """ Method to check if expected error message 
-#        "This field is required." is appeared in correct location """
-#        # a) Find all error statements on current webpage
-#        user_error_msg = self.driver.find_elements(By.XPATH, '//dev[@class="mege-error"]')
-#        # b) Check that the number of errors is equal to 1.
-#        self.assertEqual(1, len(user_error_msg))
-#        # c) Check if error message is "This is required field"
-#        self.assertEqual("This field is required.", user_error_msg)
-#        # d) Verify that the location of the error message is correct - below the First Name input field
-#        error_msg = self.driver.find_element(locate_with(By.XPATH, '//div[@class="mage-error"]').near(field_error_locator) )
-#        # e) Verify that error found using both method is the same error
-#        self.assertEqual(user_error_msg[0].id, error_msg.id)
-#
+    def requiredFieldError(self, field_error_locator):
+        """ Method to check if expected error message 
+        "This field is required." is appeared"""
+        # a) Find all error statements on current webpage
+        user_error_msg = self.driver.find_elements(*field_error_locator)
+        # b) Check that the number of errors is equal to 1.
+        self.assertEqual(1, len(user_error_msg))
+        # c) Check if error message is "This is required field"
+        self.assertEqual("This is a required field.", user_error_msg[0].text)
+
     def test_no_firstname_enter(self):
         """Test create account with no first name entered"""
         # Steps
@@ -49,38 +44,89 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        # 1. User get an information "This is required field." below First Name input field.
+        # 1. User get an information "This is required field." 
         # a) Find all error statements on current webpage
         # b) Check that the number of errors is equal to 1.
         # c) Check if error message is "This is required field"
-        # d) Verify that the location of the error message is correct - below the First Name input field
-        # e) Verify that error found using both method is the same error
-        #self.requiredFieldError(Locators.FIRST_NANE_INPUT)
-#
-#
-#    #def test_no_lastname_entered(self):
-#    #    """Test create account with no last name entered"""
-#    #    # Steps
-#    #    # 1. Click Create an Account
-#    #    # Done in setUp
-#    #    # 2. Enter first name
-#    #    self.create_account_page.enter_firstname("test")
-#    #    # 3. Enter last name
-#    #    self.create_account_page.enter_lastname("test")
-#    #    # 4. Enter email
-#    #    self.create_account_page.enter_email("test@test.pl")
-#    #    # 5. Enter Password
-#    #    self.create_account_page.enter_password("Testtest!")
-#    #    # 6. Enter Confirm Password
-#    #    self.create_account_page.enter_password_con("Testtest!")
-#    #    # 7. Click button to create account
-#    #    self.create_account_page.click_create_button()
-##
-#    #    # EXPECTED RESULT
-#        
-#
-#
-#
-#
-#
-#    
+        self.requiredFieldError(Locators.FIRST_NAME_ERROR)
+
+    def test_no_lastname_entered(self):
+        # Steps
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        # Non
+        # 4. Enter email
+        self.create_account_page.enter_email("test@test.pl")
+        # 5. Enter Password
+        self.create_account_page.enter_password("Testtest!")
+        # 6. Enter Confirm Password
+        self.create_account_page.enter_password_con("Testtest!")
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+        
+        # EXPECTED RESULT
+        self.requiredFieldError(Locators.LAST_NAME_ERROR)
+
+    def test_no_email_entered(self):
+        # Steps
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        self.create_account_page.enter_lastname("test")
+        # 4. Enter email
+        # None
+        # 5. Enter Password
+        self.create_account_page.enter_password("Testtest!")
+        # 6. Enter Confirm Password
+        self.create_account_page.enter_password_con("Testtest!")
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+        
+        # EXPECTED RESULT
+        self.requiredFieldError(Locators.EMAIL_ERROR)
+
+    def test_no_password_entered(self):
+        # Steps
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        self.create_account_page.enter_lastname("test")
+        # 4. Enter email
+        self.create_account_page.enter_email("test@test.pl")
+        # 5. Enter Password
+        # None
+        # 6. Enter Confirm Password
+        self.create_account_page.enter_password_con("Testtest!")
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+        
+        # EXPECTED RESULT
+        self.requiredFieldError(Locators.PASSWORD_ERROR)
+
+    def test_no_password_confirmation_entered(self):
+        # Steps
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        self.create_account_page.enter_lastname("test")
+        # 4. Enter email
+        self.create_account_page.enter_email("test@test.pl")
+        # 5. Enter Password
+        self.create_account_page.enter_password("Testtest!")
+        # 6. Enter Confirm Password
+        # None
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+        
+        # EXPECTED RESULT
+        self.requiredFieldError(Locators.PASSWORD_CONF_ERROR)
+
