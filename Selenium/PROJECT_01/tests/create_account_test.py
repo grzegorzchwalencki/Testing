9081c8bc78d60be59e39cb5366f6f9e335eacac0
greg-buka <bukacieszuka1@gmail.com>
2023-06-01 21:27:29 +0200

    Updated create_account_tests

--- Selenium/PROJECT_01/tests/create_account_test.py
+++ Selenium/PROJECT_01/tests/create_account_test.py
@@ -7,7 +7,7 @@ from tests.base_test import BaseTest
 from pages.create_account_page import Locators
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.common.by import By 
-from time import sleep
+
 
 class CreateAccountTest(BaseTest):
     """Create Account tests"""
@@ -15,7 +15,7 @@ class CreateAccountTest(BaseTest):
         super().setUp()
         self.create_account_page = self.home_page.click_create_account()
 
-    def requiredFieldError(self, field_error_locator):
+    def required_field_error(self, field_error_locator):
         """ Method to check if expected error message 
         "This field is required." is appeared"""
         # a) Find all error statements on current webpage
@@ -44,11 +44,11 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        # 1. User get an information "This is required field." 
+        # 1. User get an information "This is required field."
         # a) Find all error statements on current webpage
         # b) Check that the number of errors is equal to 1.
         # c) Check if error message is "This is required field"
-        self.requiredFieldError(Locators.FIRST_NAME_ERROR)
+        self.required_field_error(Locators.FIRST_NAME_ERROR)
 
     def test_no_lastname_entered(self):
         # Steps
@@ -68,7 +68,7 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        self.requiredFieldError(Locators.LAST_NAME_ERROR)
+        self.required_field_error(Locators.LAST_NAME_ERROR)
 
     def test_no_email_entered(self):
         # Steps
@@ -88,7 +88,7 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        self.requiredFieldError(Locators.EMAIL_ERROR)
+        self.required_field_error(Locators.EMAIL_ERROR)
 
     def test_no_password_entered(self):
         # Steps
@@ -108,7 +108,7 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        self.requiredFieldError(Locators.PASSWORD_ERROR)
+        self.required_field_error(Locators.PASSWORD_ERROR)
 
     def test_no_password_confirmation_entered(self):
         # Steps
@@ -128,5 +128,52 @@ class CreateAccountTest(BaseTest):
         self.create_account_page.click_create_button()
         
         # EXPECTED RESULT
-        self.requiredFieldError(Locators.PASSWORD_CONF_ERROR)
+        self.required_field_error(Locators.PASSWORD_CONF_ERROR)
 
+    def test_wrong_email_format(self):
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        self.create_account_page.enter_lastname("test")
+        # 4. Enter wrong format of email
+        self.create_account_page.enter_email("testpl")
+        # 5. Enter Password
+        self.create_account_page.enter_password("Testtest!")
+        # 6. Enter Confirm Password
+        self.create_account_page.enter_password_con("Testtest!")
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+    
+        # EXPECTED RESULT
+        # 1. User get an information "Please enter a valid email address (Ex: johndoe@domain.com)."
+        # a) Check if error appear by locator
+        user_error_msg = self.driver.find_elements(*Locators.EMAIL_ERROR)
+        # b) Check if error message is correct.
+        self.assertEqual("Please enter a valid email address (Ex: johndoe@domain.com).", user_error_msg[0].text)
+
+    def test_too_short_password_entered(self):
+        # 1. Click Create an Account
+        # Done in setUp
+        # 2. Enter first name
+        self.create_account_page.enter_firstname("test")
+        # 3. Enter last name
+        self.create_account_page.enter_lastname("test")
+        # 4. Enter wrong format of email
+        self.create_account_page.enter_email("testpl")
+        # 5. Enter Password
+        self.create_account_page.enter_password("12345")
+        # 6. Enter Confirm Password
+        self.create_account_page.enter_password_con("12345")
+        # 7. Click button to create account
+        self.create_account_page.click_create_button()
+    
+        # EXPECTED RESULT
+        # 1. User get an information "Minimum length of this field must be equal or 
+        # greater than 8 symbols. Leading and trailing spaces will be ignored."
+        expected_msg = "Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored."
+        # a) Check if error appear by locator
+        user_error_msg = self.driver.find_elements(*Locators.PASSWORD_ERROR)
+        # b) Check if error message is correct.
+        self.assertEqual(expected_msg, user_error_msg[0].text)
